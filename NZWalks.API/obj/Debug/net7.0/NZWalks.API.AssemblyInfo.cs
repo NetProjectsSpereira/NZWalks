05b785fb5691055a7d4b134433d3d6c31c8aaee5
Saul Pereira <saul_pereira@hotmail.com>
2025-08-19 16:52:21 -0600

    15. Running and Calling ASP.NET Core Web API
    15. Running and Calling ASP.NET Core Web API

--- NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5882600dc3b66f19361c5858856879e0f62a46a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
